372fdb361006690e28f3893f09e5c8547d527187
Sn7a7ke <sn7a7ke@protonmail.com>
2019-12-17 15:09:17 +0200

    Created Utility project

--- Utility/JsonUtility.cs
+++ Utility/JsonUtility.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Parser
+namespace Utility
 {
     public static class JsonUtility
     {
--- Utility/Storage.cs
+++ Utility/Storage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace Parser
+namespace Utility
 {
     public class Storage
     {
