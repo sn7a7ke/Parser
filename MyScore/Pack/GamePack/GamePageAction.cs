79b805516355a40227065a0acae45d4659478944
Sn7a7ke <sn7a7ke@protonmail.com>
2019-12-31 15:44:33 +0200

    Added GamePageAction

---
+++ MyScore/Pack/GamePack/GamePageAction.cs
@@ -0,0 +1,22 @@
+﻿using SeleniumProvider;
+
+namespace MyScore.Pack.GamePack
+{
+    public class GamePageAction
+    {
+        private readonly IWebDriverProvider _provider;
+
+        public GamePageAction(IWebDriverProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public void LiveCentre() => _provider.Click("//a[@id=\"a-match-summary\"]");
+
+        public void Timeline() => _provider.Click("//a[@id=\"a-match-timeline\"]");
+
+        public void Statistics() => _provider.Click("//a[@id=\"a-match-statistics\"]");
+
+        public void Lineups() => _provider.Click("//a[@id=\"a-match-lineups\"]");
+    }
+}
