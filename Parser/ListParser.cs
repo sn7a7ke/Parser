e66e7b8d73c0ca45cab44ef306be1e61fd7eba58
Sn7a7ke <sn7a7ke@protonmail.com>
2020-01-02 23:41:34 +0200

    Changed ListParser.Parse (heading list processing)

--- Parser/ListParser.cs
+++ Parser/ListParser.cs
@@ -7,7 +7,28 @@ namespace Parser
     public class ListParser<T> : Parser<List<T>>
     {
         private Func<HtmlNode, T> _getDesired = (n) => default(T);
+        private Predicate<HtmlNode> _isHeader = (n) => false;
+        private Func<HtmlNode, T> _getHeader = (n) => default(T);
+        private Func<T, T, T> _filling = (t1, t2) => t2;
         private Func<HtmlNode, bool> _isEnd = (n) => false;
+
+        public Func<T, T, T> Filling
+        {
+            get => _filling;
+            set => _filling = value ?? throw new NullReferenceException();
+        }
+
+        public Predicate<HtmlNode> IsHeader
+        {
+            get => _isHeader;
+            set => _isHeader = value ?? throw new NullReferenceException();
+        }
+
+        public Func<HtmlNode, T> GetHeader
+        {
+            get => _getHeader;
+            set => _getHeader = value ?? throw new NullReferenceException();
+        }
 
         public Func<HtmlNode, T> GetDesired
         {
@@ -27,14 +48,21 @@ namespace Parser
             var targetNodes = GetNodes(XPath);
             if (targetNodes == null)
                 return results;
-            T res;
+            T header = default(T);
             foreach (var node in targetNodes)
             {
                 if (IsEnd(node))
                     break;
-                res = GetDesired(node);
-                if (res != null)
+                if (IsHeader(node))
+                    header = GetHeader(node);
+                else
+                {
+                    var res = GetDesired(node);
+                    if (res != null)
+                        results.Add(res);
+                    res = Filling(header, res);
                     results.Add(res);
+                }
             }
             return results;
         }
